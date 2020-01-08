8d2455a4ebffeda25ea8e3fa530c3698f263a061
AIZAWA Hina <hina@bouhime.com>
2020-01-08 16:27:37 +0900

    Nginx 1.17.7 with LibreSSL 3.0.2

--- build.sh
+++ build.sh
@@ -2,18 +2,7 @@
 
 set -eu
 
-for i in 8; do
-  rm -rf centos${i}.build
-  docker pull oraclelinux:$i
-  make centos${i}
-  find centos${i}.build -type f -name '*.rpm' | xargs ./sign.exp
-
-  rm -f centos${i}.build/RPMS/x86_64/nginx-debuginfo-*.rpm
-  cp -f centos${i}.build/RPMS/x86_64/nginx-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/nginx-libressl/el${i}/x86_64/
-  createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/nginx-libressl/el${i}/x86_64/
-done
-
-for i in 7 6; do
+for i in 8 7 6; do
   rm -rf centos${i}.build
   docker pull centos:$i
   make centos${i}
