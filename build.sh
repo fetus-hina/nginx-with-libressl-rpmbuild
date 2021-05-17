179a87e45256e95d410a5581d4cf5b31b1cd0a76
AIZAWA Hina <hina@bouhime.com>
2021-05-18 05:54:08 +0900

    Add pgp sign for repomd.xml

--- build.sh
+++ build.sh
@@ -11,6 +11,10 @@ for i in 8 7 6; do
   rm -f centos${i}.build/RPMS/x86_64/nginx-debuginfo-*.rpm
   cp -f centos${i}.build/RPMS/x86_64/nginx-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/nginx-libressl/el${i}/x86_64/
   createrepo /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/nginx-libressl/el${i}/x86_64/
+
+  rm -f /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/nginx-libressl/el${i}/x86_64/repodata/repomd.xml.asc
+  gpg --armor --detach-sign --default-key "C9F367D2" \
+    /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/nginx-libressl/el${i}/x86_64/repodata/repomd.xml
 done
 
 for i in 5; do
@@ -22,4 +26,8 @@ for i in 5; do
   rm -f centos${i}.build/RPMS/x86_64/nginx-debuginfo-*.rpm
   cp -f centos${i}.build/RPMS/x86_64/nginx-*.rpm /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/nginx-libressl/el${i}/x86_64/
   createrepo -s sha /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/nginx-libressl/el${i}/x86_64/
+
+  rm -f /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/nginx-libressl/el${i}/x86_64/repodata/repomd.xml.asc
+  gpg --armor --detach-sign --default-key "C9F367D2" \
+    /var/www/sites/fetus.jp/rpm.fetus.jp/public_html/nginx-libressl/el${i}/x86_64/repodata/repomd.xml
 done
